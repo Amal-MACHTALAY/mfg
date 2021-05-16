22f1846f1f6465c1dd49071a2bd10e968c25610a
Amal MACHTALAY <72151716+Amal-MACHTALAY@users.noreply.github.com>
2021-05-16 19:39:12 +0100

    Update def.py

--- def.py
+++ def.py
@@ -5,39 +5,30 @@ Created on Wed Apr 28 23:41:14 2021
 @author: MACHTALAY AMAL
 """
 
-def location_int(pdf, interval, x_mfg):
-    """ generate initial locations from a given initial density pdf """
-    random_loc0=sample(pdf,interval,N)
-    loc0=np.zeros(len(random_loc0))
-    for j in range(len(random_loc0)):
-        loc0[j]=closest(x_mfg,random_loc0[j])
-    return loc0
+apt=42  # change apt to update random samples
 
+def location_int(pdf, x_mfg):
+    """ generate initial locations from a given initial density pdf """
+    random_loc=sample(pdf,N)
+    loc=np.zeros(len(random_loc))
+    for j in range(len(random_loc)):
+        loc[j]=closest(x_mfg,random_loc[j])
+    return loc
 
 def closest(lst, val):
     """ Find Closest number in a list """
     return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-val))]
 
-def inverse_sample_function(dist, pnts, x_min=-100, x_max=100, n=1e5, 
-                            **kwargs):
-        
-    x = np.linspace(x_min, x_max, int(n))
-    cumulative = np.cumsum(dist(x, **kwargs))
-    cumulative -= cumulative.min()
-    f = interp1d(cumulative/cumulative.max(), x)
-        
-    return f(np.random.random(pnts))
-
-def sample(pdf,interval,nbr_samples, n_max=10**6):
-    np.random.seed(42)
+def sample(pdf, nbr_samples, n_max=10**6):
+    np.random.seed(apt)
     """ genearte a list of random samples from a given pdf
-    suggests random samples between interval[0] and interval[1] 
+    suggests random samples between 0 and L 
     and accepts-rejects the suggestion with probability pdf(x) 
     """
     samples=[]
     n=0
     while len(samples)<nbr_samples and n<n_max:
-        x=np.random.uniform(low=interval[0],high=interval[1])
+        x=np.random.uniform(low=0,high=L)
         new_sample=pdf(x)
         assert new_sample>=0 and new_sample<=1
         if np.random.uniform(low=0,high=1) <=new_sample:
@@ -46,9 +37,25 @@ def sample(pdf,interval,nbr_samples, n_max=10**6):
     return sorted(samples)
 
 def find_index(lst, val):
+    """ find the index of val in lst"""
     ind='nan'
     for i in range(len(lst)):
         if lst[i]==val:
             ind=i
             break
     return ind
+
+def control(u_mfg, x_mfg, t_mfg):
+    X=np.zeros((N,len(t_mfg)))
+    v=np.zeros((N,len(t_mfg)))
+    X[:,0]=location_int(rho_int, x_mfg)
+    for n in t_mfg:
+        for i in range(N):
+            ind=find_index(x_mfg,X[i,n])
+            v[i,n]=u_mfg[ind,n]
+            new_x=X[i,n]+dt*v[i,n]
+            if n<N-1:
+                X[i,n+1]=closest(x_mfg,new_x)
+    return X,v
+        
+    
