183f26fdf321150a62ce9c3e18e050d4462b01d7
imadki <kissami.imad@gmail.com>
2022-01-29 16:02:27 +0100

    add initial guess

--- Traffic/petsc4py/mfg_sequential_petsc_V1.py
+++ Traffic/petsc4py/mfg_sequential_petsc_V1.py
@@ -22,7 +22,7 @@ rho_a=0.05; rho_b=0.95; gama=0.1
 mu=0.0 # viscosity coefficient 
 EPS=0.45
 ####################### grid's inputs
-Nx=40; Nt=120 # spatial-temporal grid sizes
+Nx=30; Nt=30 # spatial-temporal grid sizes
 dx=L/Nx # spatial step size
 if mu==0.0:
     dt=min(T/Nt,(CFL*dx)/u_max) # temporal step size
@@ -91,6 +91,82 @@ def f_star_p(p,r): # 0<=u<=u_max
 @njit
 def f_star(p,r): # p=Vx
     return -0.5*(p**2)+U(r)*p # MFG-LWR
+
+####################################""for interpolation""""""""""""""""""""""""""""
+def sol_to(old_Nt, old_Nx, sol,rho,u,V): # solution 1D to 2D
+    for j in range(0,old_Nx):
+        for n in range(0,old_Nt):
+            rho[j,n]=sol[j*(old_Nt+1)+n]
+            u[j,n]=sol[(old_Nt+1)*old_Nx+j*old_Nt+n]
+            V[j,n]=sol[(2*old_Nt+1)*old_Nx+j*(old_Nt+1)+n]
+        rho[j,old_Nt]=sol[j*(old_Nt+1)+old_Nt]
+        V[j,old_Nt]=sol[(2*old_Nt+1)*old_Nx+j*(old_Nt+1)+old_Nt]
+    return 0
+
+def to_sol(new_Nt, old_Nx,sol,rho,u,V):# solution 2D to 1D
+    for j in range(0,2*old_Nx):
+        for n in range(0,2*new_Nt):
+            sol[j*(2*new_Nt+1)+n]=rho[j,n]
+            sol[(2*new_Nt+1)*2*old_Nx+j*2*new_Nt+n]=u[j,n]
+            sol[(2*2*new_Nt+1)*2*old_Nx+j*(2*new_Nt+1)+n]=V[j,n]
+        sol[j*(2*new_Nt+1)+2*new_Nt]=rho[j,2*new_Nt]
+        sol[(2*2*new_Nt+1)*2*old_Nx+j*(2*new_Nt+1)+2*new_Nt]=V[j,2*new_Nt]
+    return 0
+
+
+import scipy.interpolate as interpolate
+def interpol(n,new_n,data): # 1D interpolation
+    
+    """" Go from a coarse grid Nt*Nx to a finer grid spacing (2*Nt)*(2*Nx) """""
+    i = np.indices(data.shape)[0]/(n-1)  # [0, ..., 1]
+    new_i = np.linspace(0, 1, new_n)
+    linear_interpolation_func = interpolate.interp1d(i, data, kind='cubic') 
+    # ‘linear’, ‘nearest’, ‘nearest-up’, ‘zero’, ‘slinear’, ‘quadratic’, ‘cubic’, ‘previous’, or ‘next’.
+    new_data = linear_interpolation_func(new_i)
+    return new_data
+
+def initialguess(snes, X):
+    
+    new_Nt = int(Nt/2)
+    new_Nx = int(Nx/2)
+
+    w = np.loadtxt("sol.dat")
+    
+    old_Nx = int(w[0])
+    old_Nt = int(w[1])
+    w = w[2:]
+    print(len(w))
+    
+    rho=np.zeros((old_Nx,old_Nt+1))
+    u=np.zeros((old_Nx,old_Nt))
+    V=np.zeros((old_Nx,old_Nt+1))
+    sol_to(old_Nt,old_Nx, w,rho,u,V)
+    
+    new1_rho=np.zeros((2*old_Nx,old_Nt+1))
+    new1_u=np.zeros((2*old_Nx,old_Nt))
+    new1_V=np.zeros((2*old_Nx,old_Nt+1))
+    for n in range(old_Nt):
+        new1_rho[:,n]=interpol(old_Nx,2*old_Nx,rho[:,n])
+        new1_u[:,n]=interpol(old_Nx,2*old_Nx,u[:,n])
+        new1_V[:,n]=interpol(old_Nx,2*old_Nx,V[:,n])
+    new1_rho[:,old_Nt]=interpol(old_Nx,2*old_Nx,rho[:,old_Nt])
+    new1_V[:,old_Nt]=interpol(old_Nx,2*old_Nx,V[:,old_Nt])
+    new_rho=np.zeros((2*old_Nx,2*new_Nt+1))
+    new_u=np.zeros((2*old_Nx,2*new_Nt))
+    new_V=np.zeros((2*old_Nx,2*new_Nt+1))
+    for j in range(2*old_Nx):
+        new_rho[j,:]=interpol(old_Nt+1,2*new_Nt+1,new1_rho[j,:])
+        new_u[j,:]=interpol(old_Nt,2*new_Nt,new1_u[j,:])
+        new_V[j,:]=interpol(old_Nt+1,2*new_Nt+1,new1_V[j,:])
+        
+    new_w = np.zeros(3*(2*new_Nt)*(2*old_Nx)+2*(2*old_Nx))
+    to_sol(new_Nt, old_Nx, new_w,new_rho,new_u,new_V)
+  
+    X = new_w
+    
+    # print(X)
+    
+    return X
 
 def formFunction(snes, w, F):
     FF = F.array
@@ -165,81 +241,22 @@ def formJacobian(snes, w, J, P):
     for j in range(1,Nx+1):
         row.append(Frint_idx(j)); col.append(r_idx(j,0)); data.append(1)
         row.append(FVter_idx(j)); col.append(V_idx(j,Nt)); data.append(1)
+    
+    
     P.setType("mpiaij")
     P.setFromOptions()
-    
     P.setPreallocationNNZ(10)
-    P.setOption(option=19, flag=0)
+    # P.setOption(option=19, flag=0)
     
     for i in range(len(data)):
         P.setValues(row[i], col[i], data[i], addv=False)
-
+    
     P.assemble()
     if J != P:
         J.assemble()
             
     return PETSc.Mat.Structure.SAME_NONZERO_PATTERN
 
-####################################""for interpolation""""""""""""""""""""""""""""
-def sol_to(n_Nt,sol,rho,u,V):
-    for j in range(0,Nx):
-        for n in range(0,n_Nt):
-            rho[j,n]=sol[j*(n_Nt+1)+n]
-            u[j,n]=sol[(n_Nt+1)*Nx+j*n_Nt+n]
-            V[j,n]=sol[(2*n_Nt+1)*Nx+j*(n_Nt+1)+n]
-        rho[j,n_Nt]=sol[j*(n_Nt+1)+n_Nt]
-        V[j,n_Nt]=sol[(2*n_Nt+1)*Nx+j*(n_Nt+1)+n_Nt]
-    return 0
-
-def to_sol(n_Nt,sol,rho,u,V):
-    for j in range(0,2*Nx):
-        for n in range(0,2*n_Nt):
-            sol[j*(2*n_Nt+1)+n]=rho[j,n]
-            sol[(2*n_Nt+1)*2*Nx+j*2*n_Nt+n]=u[j,n]
-            sol[(2*2*n_Nt+1)*2*Nx+j*(2*n_Nt+1)+n]=V[j,n]
-        sol[j*(2*n_Nt+1)+2*n_Nt]=rho[j,2*n_Nt]
-        sol[(2*2*n_Nt+1)*2*Nx+j*(2*n_Nt+1)+2*n_Nt]=V[j,2*n_Nt]
-    return 0
-
-
-import scipy.interpolate as interpolate
-def interpol(n,new_n,data): # 1D interpolation
-    
-    """" Go from a coarse grid Nt*Nx to a finer grid spacing (2*Nt)*(2*Nx) """""
-    i = np.indices(data.shape)[0]/(n-1)  # [0, ..., 1]
-    new_i = np.linspace(0, 1, new_n)
-    linear_interpolation_func = interpolate.interp1d(i, data, kind='cubic') 
-    # ‘linear’, ‘nearest’, ‘nearest-up’, ‘zero’, ‘slinear’, ‘quadratic’, ‘cubic’, ‘previous’, or ‘next’.
-    new_data = linear_interpolation_func(new_i)
-    return new_data
-
-def multigrid(old_Nt,new_Nt,w):
-    rho=np.zeros((Nx,old_Nt+1))
-    u=np.zeros((Nx,old_Nt))
-    V=np.zeros((Nx,old_Nt+1))
-    sol_to(old_Nt,w,rho,u,V)
-    new1_rho=np.zeros((2*Nx,old_Nt+1))
-    new1_u=np.zeros((2*Nx,old_Nt))
-    new1_V=np.zeros((2*Nx,old_Nt+1))
-    for n in range(old_Nt):
-        new1_rho[:,n]=interpol(Nx,2*Nx,rho[:,n])
-        new1_u[:,n]=interpol(Nx,2*Nx,u[:,n])
-        new1_V[:,n]=interpol(Nx,2*Nx,V[:,n])
-    new1_rho[:,old_Nt]=interpol(Nx,2*Nx,rho[:,old_Nt])
-    new1_V[:,old_Nt]=interpol(Nx,2*Nx,V[:,old_Nt])
-    new_rho=np.zeros((2*Nx,2*new_Nt+1))
-    new_u=np.zeros((2*Nx,2*new_Nt))
-    new_V=np.zeros((2*Nx,2*new_Nt+1))
-    for j in range(2*Nx):
-        new_rho[j,:]=interpol(old_Nt+1,2*new_Nt+1,new1_rho[j,:])
-        new_u[j,:]=interpol(old_Nt,2*new_Nt,new1_u[j,:])
-        new_V[j,:]=interpol(old_Nt+1,2*new_Nt+1,new1_V[j,:])
-        
-    new_w = np.zeros(3*(2*new_Nt)*(2*Nx)+2*(2*Nx))
-    to_sol(new_Nt,new_w,new_rho,new_u,new_V)
-    
-    return new_w
-####################################################################
  
 # """************************ solve in grid 1***************************** """
 from petsc4py import PETSc
@@ -263,26 +280,29 @@ J.setSizes(shap)
 J.setFromOptions()
 J.setUp()
 
-w = np.zeros(3*Nt*Nx+2*Nx)
+# w = np.zeros(3*Nt*Nx+2*Nx)
 
 snes.setFunction(formFunction, F)
 snes.setJacobian(formJacobian)
-
-snes.getKSP().setType('fgmres')
-
-snes.setFromOptions()
+snes.setInitialGuess(initialguess)
 
 
+snes.getKSP().setType('cg')
+# snes.setFromOptions()
 
 ksp = snes.getKSP()
 pc = ksp.getPC()
+pc.setFactorSolverType("mumps")
 opts = PETSc.Options()
 opts["ksp_rtol"] = 1.0e-6
 opts["pc_type"] = "lu"
+ksp.setInitialGuessNonzero(True)
 ksp.setFromOptions()
 
 snes.setTolerances(rtol = 1e-6)
 snes.setFromOptions()
+
+
 
 t0 = time.process_time()   ###
 snes.solve(b, xx)
@@ -301,82 +321,11 @@ litspit = lits/float(its)
 print ("Average Linear its / SNES = %e", float(litspit))
 
 
-# """************************ solve in grid 2***************************** """
-Nxx=2*Nx; Ntt=2*Nt
-# print('Nx={Nx}, Nt={Nt}'.format(Nx=Nxx,Nt=Ntt))
-dx=L/Nxx # spatial step size
-# print('dx={dx}, dt={dt}'.format(dx=round(dx,3),dt=round(dt,3)))
-if mu==0.0:
-    dt=min(T/Ntt,(CFL*dx)/u_max) # temporal step size
-    eps=0.0
-else:
-    dt=min(T/Ntt,CFL*dx/abs(u_max),EPS*(dx**2)/mu) # temporal step size
-    eps=mu*dt/(dx**2) # V
-x=np.linspace(0,L,Nxx+1)
-# t=np.linspace(0,T,Ntt+1)
-t=np.arange(0,T+dt,dt)
-Nt=int((len(t)-1)/2)
-# t0 = time.process_time()   ###
-guess=multigrid(int(Ntt/2),Nt,xx)
-# t1 = time.process_time()   ###
-# print("Time spent (multigrid) :",t1-t0)
 
-
-from petsc4py import PETSc
-
-shap=(3*Nt*Nx+2*Nx,3*Nt*Nx+2*Nx)
-
-# create nonlinear solver
-snes = PETSc.SNES()
-snes.create()
-
-F = PETSc.Vec()
-F.create()
-F.setSizes(shap[0])
-F.setFromOptions()
-
-b = None
-xx = PETSc.Vec().createSeq(shap[0]) 
-
-J = PETSc.Mat().create()
-J.setSizes(shap)
-J.setFromOptions()
-J.setUp()
-
-w = np.zeros(3*Nt*Nx+2*Nx)
-
-snes.setFunction(formFunction, F)
-snes.setJacobian(formJacobian)
-
-snes.getKSP().setType('fgmres')
-
-snes.setFromOptions()
-
-
-
-ksp = snes.getKSP()
-pc = ksp.getPC()
-opts = PETSc.Options()
-opts["ksp_rtol"] = 1.0e-6
-opts["pc_type"] = "lu"
-ksp.setFromOptions()
-
-snes.setTolerances(rtol = 1e-6)
-snes.setFromOptions()
-
-t0 = time.process_time()   ###
-snes.solve(b, xx)
-t1 = time.process_time()   ###
-time2=t1-t0
-print("Time spent:",time2)
-
-
-its = snes.getIterationNumber()
-lits = snes.getLinearSolveIterations()
-
-print ("Number of SNES iterations = :", its)
-print ("Number of Linear iterations =" , lits)
-
-litspit = lits/float(its)
-print ("Average Linear its / SNES = %e", float(litspit))
-
+filename = ("sol.dat")
+with open(filename, "a") as text_file:
+    text_file.write(str(Nx))
+    text_file.write("\n")
+    text_file.write(str(Nt))
+    text_file.write("\n")
+    np.savetxt(text_file, xx.array)
