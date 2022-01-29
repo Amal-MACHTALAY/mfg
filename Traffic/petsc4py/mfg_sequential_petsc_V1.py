4c7e0c62a2ae0b730affa7689b4074f487814e98
Amal MACHTALAY <72151716+Amal-MACHTALAY@users.noreply.github.com>
2022-01-29 21:49:30 +0100

    Update mfg_sequential_petsc_V1.py

--- Traffic/petsc4py/mfg_sequential_petsc_V1.py
+++ Traffic/petsc4py/mfg_sequential_petsc_V1.py
@@ -22,7 +22,8 @@ rho_a=0.05; rho_b=0.95; gama=0.1
 mu=0.0 # viscosity coefficient 
 EPS=0.45
 ####################### grid's inputs
-Nx=40; Nt=30; use_interp = 1 # spatial-temporal grid sizes, use interpolation
+multip=3 # mutiple for interpolation
+Nx=15*multip; Nt=60*multip; use_interp = 1 # spatial-temporal grid sizes, use interpolation
 dx=L/Nx # spatial step size
 if mu==0.0:
     dt=min(T/Nt,(CFL*dx)/u_max) # temporal step size
@@ -104,13 +105,13 @@ def sol_to(old_Nt, old_Nx, sol,rho,u,V): # solution 1D to 2D
     return 0
 
 def to_sol(new_Nt, old_Nx,sol,rho,u,V):# solution 2D to 1D
-    for j in range(0,2*old_Nx):
-        for n in range(0,2*new_Nt):
-            sol[j*(2*new_Nt+1)+n]=rho[j,n]
-            sol[(2*new_Nt+1)*2*old_Nx+j*2*new_Nt+n]=u[j,n]
-            sol[(2*2*new_Nt+1)*2*old_Nx+j*(2*new_Nt+1)+n]=V[j,n]
-        sol[j*(2*new_Nt+1)+2*new_Nt]=rho[j,2*new_Nt]
-        sol[(2*2*new_Nt+1)*2*old_Nx+j*(2*new_Nt+1)+2*new_Nt]=V[j,2*new_Nt]
+    for j in range(0,multip*old_Nx):
+        for n in range(0,multip*new_Nt):
+            sol[j*(multip*new_Nt+1)+n]=rho[j,n]
+            sol[(multip*new_Nt+1)*multip*old_Nx+j*multip*new_Nt+n]=u[j,n]
+            sol[(2*multip*new_Nt+1)*multip*old_Nx+j*(multip*new_Nt+1)+n]=V[j,n]
+        sol[j*(multip*new_Nt+1)+multip*new_Nt]=rho[j,multip*new_Nt]
+        sol[(2*multip*new_Nt+1)*multip*old_Nx+j*(multip*new_Nt+1)+multip*new_Nt]=V[j,multip*new_Nt]
     return 0
 
 
@@ -127,9 +128,9 @@ def interpol(n,new_n,data): # 1D interpolation
 
 def initialguess(X):
     
-    new_Nt = int(Nt/2)
+    new_Nt = int(Nt/multip)
 
-    w = np.loadtxt("sol.dat")
+    w = np.loadtxt("sol_15_60_mfg_sequential_petsc_V1.dat")
     
     old_Nx = int(w[0])
     old_Nt = int(w[1])
@@ -141,24 +142,24 @@ def initialguess(X):
     V=np.zeros((old_Nx,old_Nt+1))
     sol_to(old_Nt,old_Nx, w,rho,u,V)
     
-    new1_rho=np.zeros((2*old_Nx,old_Nt+1))
-    new1_u=np.zeros((2*old_Nx,old_Nt))
-    new1_V=np.zeros((2*old_Nx,old_Nt+1))
+    new1_rho=np.zeros((multip*old_Nx,old_Nt+1))
+    new1_u=np.zeros((multip*old_Nx,old_Nt))
+    new1_V=np.zeros((multip*old_Nx,old_Nt+1))
     for n in range(old_Nt):
-        new1_rho[:,n]=interpol(old_Nx,2*old_Nx,rho[:,n])
-        new1_u[:,n]=interpol(old_Nx,2*old_Nx,u[:,n])
-        new1_V[:,n]=interpol(old_Nx,2*old_Nx,V[:,n])
-    new1_rho[:,old_Nt]=interpol(old_Nx,2*old_Nx,rho[:,old_Nt])
-    new1_V[:,old_Nt]=interpol(old_Nx,2*old_Nx,V[:,old_Nt])
-    new_rho=np.zeros((2*old_Nx,2*new_Nt+1))
-    new_u=np.zeros((2*old_Nx,2*new_Nt))
-    new_V=np.zeros((2*old_Nx,2*new_Nt+1))
-    for j in range(2*old_Nx):
-        new_rho[j,:]=interpol(old_Nt+1,2*new_Nt+1,new1_rho[j,:])
-        new_u[j,:]=interpol(old_Nt,2*new_Nt,new1_u[j,:])
-        new_V[j,:]=interpol(old_Nt+1,2*new_Nt+1,new1_V[j,:])
+        new1_rho[:,n]=interpol(old_Nx,multip*old_Nx,rho[:,n])
+        new1_u[:,n]=interpol(old_Nx,multip*old_Nx,u[:,n])
+        new1_V[:,n]=interpol(old_Nx,multip*old_Nx,V[:,n])
+    new1_rho[:,old_Nt]=interpol(old_Nx,multip*old_Nx,rho[:,old_Nt])
+    new1_V[:,old_Nt]=interpol(old_Nx,multip*old_Nx,V[:,old_Nt])
+    new_rho=np.zeros((multip*old_Nx,multip*new_Nt+1))
+    new_u=np.zeros((multip*old_Nx,multip*new_Nt))
+    new_V=np.zeros((multip*old_Nx,multip*new_Nt+1))
+    for j in range(multip*old_Nx):
+        new_rho[j,:]=interpol(old_Nt+1,multip*new_Nt+1,new1_rho[j,:])
+        new_u[j,:]=interpol(old_Nt,multip*new_Nt,new1_u[j,:])
+        new_V[j,:]=interpol(old_Nt+1,multip*new_Nt+1,new1_V[j,:])
         
-    new_w = np.zeros(3*(2*new_Nt)*(2*old_Nx)+2*(2*old_Nx))
+    new_w = np.zeros(3*(multip*new_Nt)*(multip*old_Nx)+2*(multip*old_Nx))
     to_sol(new_Nt, old_Nx, new_w,new_rho,new_u,new_V)
   
     X = new_w
